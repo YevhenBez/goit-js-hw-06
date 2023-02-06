787bb4b400a383c69df9c0689c74d056f512eaad
YevhenBez <byxgalter@bigmir.net>
2023-02-06 20:49:20 +0200

    Задание 1 Домашки №6 по JS полностью сделано
    Задание 1 Домашки №6 по JS полностью сделано

--- js/task-01.js
+++ js/task-01.js
@@ -13,4 +13,24 @@ console.log(`Category: ${titleAnimals}`);
 const secondListAnimals = categoriesLiForAnimals.lastElementChild;
 
 const numbersSecondListAnimals = secondListAnimals.children.length;
-console.log(`Elements: ${ numbersSecondListAnimals }`);
+console.log(`Elements: ${numbersSecondListAnimals}`);
+
+const categoriesLiForProducts = categoriesLiForAnimals.nextElementSibling;
+
+const titleProducts = categoriesLiForProducts.firstElementChild.textContent;
+console.log(`Category: ${titleProducts}`);
+
+const secondListProducts = categoriesLiForProducts.lastElementChild;
+
+const numbersSecondListProducts = secondListProducts.children.length;
+console.log(`Elements: ${numbersSecondListProducts}`);
+
+const categoriesLiForTechnologies = categoriesLiForProducts.nextElementSibling;
+
+const titleTechnologies = categoriesLiForTechnologies.firstElementChild.textContent;
+console.log(`Category: ${titleTechnologies}`);
+
+const secondListTechnologies = categoriesLiForTechnologies.lastElementChild;
+
+const numbersSecondListTechnologies = secondListTechnologies.children.length;
+console.log(`Elements: ${numbersSecondListTechnologies}`);
