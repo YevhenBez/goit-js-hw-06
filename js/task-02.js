018a579ada1ff213295129aa8a79188626fc5115
YevhenBez <byxgalter@bigmir.net>
2023-02-06 23:41:32 +0200

    Сделал Задание 2 для Домашки №6 по JS
    Сделал Задание 2 для Домашки №6 по JS  . Если я данный код не обновлю  - то это решение данной задачи верное.

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,31 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const firstLi = document.createElement('li');
+firstLi.textContent = ingredients[0];
+firstLi.classList.add('item');
+
+const secondLi = document.createElement('li');
+secondLi.textContent = ingredients[1];
+secondLi.classList.add('item');
+
+const thirdLi = document.createElement('li');
+thirdLi.textContent = ingredients[2];
+thirdLi.classList.add('item');
+
+const fourthLi = document.createElement('li');
+fourthLi.textContent = ingredients[3];
+fourthLi.classList.add('item');
+
+const fifthLi = document.createElement('li');
+fifthLi.textContent = ingredients[4];
+fifthLi.classList.add('item');
+
+const sixthLi = document.createElement('li');
+sixthLi.textContent = ingredients[5];
+sixthLi.classList.add('item');
+
+const list = document.querySelector('#ingredients');
+
+list.append(firstLi, secondLi, thirdLi, fourthLi, fifthLi, sixthLi);
