573b52d37963264d0ab19e0dca1d25a3295b8bac
YevhenBez <byxgalter@bigmir.net>
2023-02-09 00:30:03 +0200

    Полностью сделанное задание 3 Домашки №6 по JS
    Полностью сделанное задание 3 Домашки №6 по JS

--- js/task-03.js
+++ js/task-03.js
@@ -16,7 +16,7 @@ const images = [
 
 const galleryListEl = document.querySelector('.gallery');
 
-const insertGalleryItems = images.map((image) => `<li><img src=${image.url} alt=${image.alt}></li>`);
+const insertGalleryItems = images.map((image) => `<li><img src="${image.url}" alt="${image.alt}" width="100%" height="100%"></li>`).join("");
 galleryListEl.insertAdjacentHTML("beforeend", insertGalleryItems);
 
 console.log(galleryListEl);
