9e0fca18b523f81a20ec68bde3143df5de36d5b6
YevhenBez <byxgalter@bigmir.net>
2023-02-13 00:14:18 +0200

    Простой метод решения задания 8 Домашки 6 по JS
    Простой метод решения задания 8 Домашки 6 по JS

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,27 @@
+const form = document.querySelector('.login-form');
 
+const message = "Всі поля повинні бути заповнені!";
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const mail = event.currentTarget.elements.email.value;
+
+    const passwordKey = event.currentTarget.elements.password.value;
+
+    if (mail === "" || passwordKey === "") {
+        alert(message);
+    }
+    
+    const mailAndPassword = {
+
+    };
+
+    mailAndPassword.email = mail;
+
+    mailAndPassword.password = passwordKey;
+
+    console.log(mailAndPassword);
+}
