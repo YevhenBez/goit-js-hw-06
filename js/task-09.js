fc3d1155aae2ffd68cbb6bd4cf105ea04cc0e9bc
YevhenBez <byxgalter@bigmir.net>
2023-02-13 20:14:53 +0200

    Готовое задание 9 Домашки №6 по JS
    Готовое задание 9 Домашки №6 по JS

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const spanColor = document.querySelector('.color');
+
+const buttonChangeColor = document.querySelector('.change-color');
+
+buttonChangeColor.addEventListener('click', event => {
+  spanColor.style.backgroundColor = getRandomHexColor();
+});
