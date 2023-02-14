06499e7b8189d1698f5e6e3fdeb018f12f37b9c1
YevhenBez <byxgalter@bigmir.net>
2023-02-14 21:22:46 +0200

    Решённое задание 6 Домашки №6 по JS
    Решённое задание 6 Домашки №6 по JS - решено при помощи замены классов

--- js/task-06.js
+++ js/task-06.js
@@ -5,8 +5,15 @@ textInput.addEventListener('blur', event => {
 
     const numberCharactersValid = Number.parseInt(textInput.data - length);
     if (charactersEntered.length === Number.parseInt(textInput.getAttribute('data-length'))) {
-    textInput.style.borderColor = "#4caf50";
-} else {
-        textInput.style.borderColor = "#f44336";
+
+        textInput.classList.add('valid');
+		if (textInput.classList.contains('invalid')) {
+            textInput.classList.remove('invalid');
+		}
+	} else {
+		if (textInput.classList.contains('valid')) {
+            textInput.classList.remove('valid');
+		}
+        textInput.classList.add('invalid');
 }
 })
